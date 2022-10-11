0916730c66c594a2a24be0f0fc0b10a5e1272a56
HongSeungBo <107994618+HongSeungBo@users.noreply.github.com>
2022-10-11 20:05:12 +0900

    second

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-$(map).on("click", function(){
-    if(onclick){
-        mapdown
-    }else if(map != onclick)
-});
+$(document).ready(function(){
+
+    $('#map').on('click', function(){
+        if($("#map").hasClass("mapSizeDown")){
+            $("#map").removeClass("mapSizeDown");
+            $("#map").addClass("mapSizeUp");
+            $("#mainSection").css("display", 'none')
+        } else {
+            $("#map").removeClass("mapSizeUp");
+            $("#map").addClass("mapSizeDown");
+            $("#mainSection").css("display", '')
+        }   
+    })
+})
