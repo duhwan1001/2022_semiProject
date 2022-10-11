0a33ea50454c9567d37a354f32595549dedf7914
duhwan1001 <kdhz2012@gmail.com>
2022-10-11 18:23:52 +0900

    221011_commit

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 
+
+
 $(document).ready(function(){
 
-    var mapFlag = 0;
     $('#map').on('click', function(){
-        if(mapFlag === 0){
-            $('#map').width(200);
-            $('#map').height(300);
-            $("#map").css("margin-left", 50);
-            mapFlag = 1;
+        if($("#map").hasClass("mapSizeDown")){
+            $("#map").removeClass("mapSizeDown");
+            $("#map").addClass("mapSizeUp");
+            $("#mainSection").css("display", 'none')
         } else {
-            $('#map').width(650);
-            $('#map').height(761);
-            $("#map").css("margin-left", 150);
-            mapFlag = 0;
-        }
+            $("#map").removeClass("mapSizeUp");
+            $("#map").addClass("mapSizeDown");
+            $("#mainSection").css("display", '')
+        }   
+        
+        
+
     })
-    
-
-
-
 })
