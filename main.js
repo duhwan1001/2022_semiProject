60d8bc95d3935f35e68ba87706bcdd16c4ab4e9c
duhwan1001 <kdhz2012@gmail.com>
2022-10-12 18:21:31 +0900

    main, left aside animation, main pin img, text

--- main.js
+++ main.js
@@ -7,14 +7,39 @@ $(document).ready(function(){
         if($("#map").hasClass("mapSizeDown")){
             $("#map").removeClass("mapSizeDown");
             $("#map").addClass("mapSizeUp");
+            $("#mainSection").removeClass("fadeInPage");
+            $("#mainSection").addClass("fadeOutPage");
+            $("#cityList").removeClass("fadeInPage");
+            $("#cityList").addClass("fadeOutPage");
+  
             $("#mainSection").css("display", 'none')
+            $("#cityList").css("display", 'none')
         } else {
             $("#map").removeClass("mapSizeUp");
             $("#map").addClass("mapSizeDown");
+            $("#mainSection").removeClass("fadeOutPage");
+            $("#mainSection").addClass("fadeInPage");
+            $("#cityList").removeClass("fadeOutPage");
+            $("#cityList").addClass("fadeInPage");
             $("#mainSection").css("display", '')
+            $("#cityList").css("display", '')
         }   
-        
-        
+    })
+
+    // pinSections
+    $('#pinSection1').on('click', function(){
+        alert("서울!");
+        console.log($("#mainIframe").attr("src"));
+        $("#mainIframe").attr("src", "./citys/seoul.html");
+        console.log($("#mainIframe").attr("src"));
 
     })
+    $('#pinSection2').on('click', function(){
+        alert("서울!");
+    })
+    $('#pinSection3').on('click', function(){
+        alert("서울!");
+    })
+
+    //    $(document).mousemove(function(e){      console.log(e.pageX + ', ' + e.pageY);   });
 })
