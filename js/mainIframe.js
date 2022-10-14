18690b5673115080b876111764ed5d19e0ac557b
duhwan1001 <kdhz2012@gmail.com>
2022-10-14 22:41:35 +0900

    AnJinHyuck Folder deleted, update files

--- js/mainIframe.js
+++ js/mainIframe.js
@@ -9,9 +9,13 @@ $(document).ready(function(){
     li.setAttribute('id', cityNameEn);
     li.setAttribute('value', link);
     li.setAttribute('class', 'city');
-    li.innerHTML = cityNameKr;
-
+    li.innerHTML = cityNameKr + "<i style='cursor : pointer' id='deleteBookmark' class='fa fa-trash' aria-hidden='true'></i>";
+    // 쓰레기 통 아이콘 박았으나 기능 미구현
+    
+    var citiesList = window.parent.document.getElementById("cities");
     $('#bookMark').on('click', function(){
-        window.parent.document.getElementById("cities").appendChild(li); 
+        $("#id", parent.document.getElementById("cities").appendChild(li));
+        
+        // 즐겨찾기 기능인데 삭제는 미구현
     });
 })
